d470a134573ded00d248dc203510d0b4b638f386
dtshering <dukpa99@gmail.com>
2025-02-06 15:36:08 +0600

    push q5

--- 5_optimal_skills.sql
+++ 5_optimal_skills.sql
@@ -17,7 +17,7 @@ JOIN job_skills ON job_posting.job_id = job_skills.job_id
 JOIN skills ON job_skills.skill_id = skills.skill_id
 WHERE job_title_short = 'Data Analyst' AND
       salary_year_avg IS NOT NULL AND
-      job_work_from_home = 'True'
+      job_work_from_home = True
 GROUP BY skills.skill_id, skills.skills
 ), average_salary AS(
 SELECT 
@@ -30,7 +30,6 @@ WHERE job_title_short = 'Data Analyst' AND
       salary_year_avg IS NOT NULL AND 
       job_work_from_home = 'True'
 GROUP BY job_skills.skill_id,skills.skills
-
 )
 SELECT 
       skills_in_demand.skill_id,
@@ -44,3 +43,26 @@ ORDER BY
          avg_salary DESC,
          count_in_demand DESC
 LIMIT 30
+
+--optional
+
+SELECT
+      skills.skill_id,
+      skills.skills,
+      COUNT(job_skills.job_id) AS count_in_demand,
+      ROUND(AVG(CAST(salary_year_avg AS DECIMAL(10,2))), 0) AS avg_salary
+FROM job_posting
+JOIN job_skills ON job_posting.job_id = job_skills.job_id
+JOIN skills ON job_skills.skill_id = skills.skill_id
+WHERE
+      job_title_short = 'Data Analyst'
+      AND salary_year_avg IS NOT NULL
+      AND job_work_from_home = True
+GROUP BY
+      skills.skill_id, skills.skills
+HAVING
+      COUNT(job_skills.job_id)>10
+ORDER BY
+      avg_salary DESC,
+      count_in_demand DESC
+LIMIT 30;
